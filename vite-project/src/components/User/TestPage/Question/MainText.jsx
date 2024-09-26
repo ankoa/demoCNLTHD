b20e9bf30ce02b69cdbac7dcca40625fad4c9217
Nguyen Anh Khoa <akhoa0805@gmail.com>
2024-09-26 08:12:22 +0700

    fix root

---
+++ vite-project/src/components/User/TestPage/Question/MainText.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+import './MainText.scss';
+const TestPage = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default TestPage;
---
+++ vite-project/src/util/router.jsx
@@ -0,0 +1,7 @@
+export const ROUTERS = {
+    USER: {
+        HOMEPAGE: "/",
+        TESTPAGE: "/test",
+        LOGIN: "/login"
+    }
+};
