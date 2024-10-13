5fa912388809163b95ad54dbfa93ed14d5502224
Nguyen Anh Khoa <akhoa0805@gmail.com>
2024-10-13 23:19:40 +0700

    commit 13/10

--- src/components/User/Header/Header.jsx
+++ src/components/User/Header/Header.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
-
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { FaPhoneAlt } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
-
 import "./Header.scss";
 
 const Header = () => {
@@ -11,18 +11,16 @@ const Header = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
   const [showUpperHeader, setShowUpperHeader] = useState(true);
   const [addDropShadow, setAddDropShadow] = useState(false);
-  const navigate = useNavigate();
-
   const handleScroll = () => {
     const currentScrollPos = window.pageYOffset;
     setScrollPosition(currentScrollPos);
 
     if (currentScrollPos > 300) {
-      setShowUpperHeader(false); // Ẩn upper-header
-      setAddDropShadow(true); // Thêm hiệu ứng đổ bóng
+      setShowUpperHeader(false); 
+      setAddDropShadow(true);    
     } else {
-      setShowUpperHeader(true); // Hiển thị lại upper-header
-      setAddDropShadow(false); // Bỏ hiệu ứng đổ bóng
+      setShowUpperHeader(true);   
+      setAddDropShadow(false);    
     }
   };
 
@@ -34,49 +32,31 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="header-container">
+    <div className='header-container'>
       {showUpperHeader && (
-        <div className="upper-header d-flex justify-content-evenly">
-          <div className="phone-number">
-            <FaPhoneAlt /> 0123456789
-          </div>
-          <div className="header-banner">Thi toeic 990đ quá dễ</div>
-          <div className="login-option">
+        <div className='upper-header d-flex justify-content-evenly'>
+          <div className='phone-number'><FaPhoneAlt /> 0123456789</div>
+          <div className='header-banner'>Thi toeic 990đ quá dễ</div>
+          <div className='login-option'>
             {isLogin ? (
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
               </NavDropdown>
             ) : (
-              <div className="auth-container d-flex gap-3">
-                <button
-                  className="btn btn-primary"
-                  onClick={() => navigate("/Login")}
-                >
-                  Login
-                </button>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => navigate("/Register")}
-                >
-                  Sign up
-                </button>
+              <div className='auth-container d-flex gap-3'>
+                <button className='btn btn-primary'>Login</button>
+                <button className='btn btn-primary'>Sign up</button>
               </div>
             )}
           </div>
         </div>
       )}
 
-      {/* <div className={`main-header ${addDropShadow ? 'shadow' : ''}`}>
+      <div className={`main-header ${addDropShadow ? 'shadow' : ''}`}>
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
@@ -96,9 +76,9 @@ const Header = () => {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-      </div> */}
+      </div>
     </div>
   );
-};
+}
 
 export default Header;
